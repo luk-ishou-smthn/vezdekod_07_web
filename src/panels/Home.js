262b2967271cc844c39764f4c957c92adfd3d080
vldplcd <shilov.part@gmail.com>
2020-09-14 01:57:07 +0300

    Start page started

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -11,23 +11,24 @@ import Placeholder from '@vkontakte/vkui/dist/components/Placeholder/Placeholder
 
 
 const Home = ({ id, go, fetchedUser }) => (
-	<Panel id={id}
-		style={{
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}}>
+	<Panel id={id}>
 		<PanelHeader>Пожертвования</PanelHeader>
-		<Placeholder
+		<Div style={{display: 'flex',
+			    alignItems: 'center',
+			    justifyContent: 'center',}}>
+			<Placeholder
             //>icon={<Icon56UsersOutline />}
             //>header="Уведомления от сообществ"
             action={<Button size='l' onClick={go} data-to="persik">
 					Создать сбор
 				</Button>}
-          >
+				>
             {`У вас пока нет сборов.
             Начните доброе дело`}
-          </Placeholder>
+         	</Placeholder>
+		</Div>
+		
+		
 
 	</Panel>
 );
